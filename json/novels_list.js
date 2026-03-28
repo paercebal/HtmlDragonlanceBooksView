c7f6ce540e114225f66008b9bfbd9ceaa2e37739
Raoul Borges <paercebal@paercebal.org>
2026-03-28 18:52:22 +0100

    First draft

---
+++ json/novels_list.js
@@ -0,0 +1,68 @@
+const g_novels_list =
+[
+   {
+      "name" : "Chronicles"
+      , "why" : "Must read first."
+      , "books" :
+      [
+         { "name" : "Dragons of Autumn Twilight" }
+         , { "name" : "Dragons of Winter Night" }
+         , { "name" : "Dragons of Spring Dawning" }
+      ]
+   }
+   ,
+   {
+      "name" : "Legends"
+      , "why" : "Must read if you enjoyed the Chronicles."
+      , "books" :
+      [
+         { "name" : "Time of the Twins" }
+         , { "name" : "War of the Twins" }
+         , { "name" : "Test of the Twins" }
+      ]
+   }
+   ,
+   {
+      "name" : "Lost Chronicles"
+      , "why" : "If you want to know what happened between the Chronicles."
+      , "books" :
+      [
+         { "name" : "Dragons of the Dwarven Depths" }
+         , { "name" : "Dragons of the Highlord Skies" }
+         , { "name" : "Dragons of the Hourglass Mage" }
+      ]
+   }
+   ,
+   {
+      "name" : "Raistlin Chronicles"
+      , "why" : "If you want to know what happened to Raistlin and Caramon before the War of the Lance."
+      , "books" :
+      [
+         { "name" : "The Soulforge" }
+         , { "name" : "Brothers in Arms" }
+      ]
+   }
+   ,
+   {
+      "name" : "Elven Nations"
+      , "why" : "If you want to know the origin of the split between the Qualinesti and the Silvanesti."
+      , "books" :
+      [
+         { "name" : "Firstborn" }
+         , { "name" : "The Kinslayer Wars" }
+         , { "name" : "The Qualinesti" }
+      ]
+   }
+   ,
+   {
+      "name" : "Kingpriest"
+      , "why" : "If you want to know what led to the Cataclysm."
+      , "books" :
+      [
+         { "name" : "Chosen of the Gods" }
+         , { "name" : "Divine Hammer" }
+         , { "name" : "Sacred Fire" }
+      ]
+   }
+   ,
+];
