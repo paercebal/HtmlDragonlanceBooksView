7033e86c65b77461ce41802d773c52ea386a18b2
Raoul Borges <paercebal@paercebal.org>
2026-03-28 19:51:58 +0100

    Add and use functions: list_to_map, create_svg_foreign_object

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,32 @@ function id(p_id)
    return output;
 }
 
+function list_to_map(p_list, p_property)
+{
+   let map = {};
+
+   p_list.forEach(
+         (p_item) =>
+         {
+            const name = p_item[p_property];
+            map[name] = p_item;
+         }
+      );
+
+   return map;
+}
+
+function create_svg_foreign_object(p_center_x, p_center_y, p_width, p_height, p_content, p_css_class)
+{
+   const css_class = ((p_css_class === undefined) || (p_css_class == null)) ? (``) : (`class="${p_css_class}"`);
+   
+   return `<foreignObject x="${p_center_x - p_width / 2}" y="${p_center_y -p_height / 2}" width="${p_width}" height="${p_height}">`
+   + `<div xmlns="http://www.w3.org/1999/xhtml" style="padding:0 auto"><div ${css_class}>${p_content}</div></div>`
+   + `</foreignObject>`;
+}
+
+
+
 
 const g_book_width = 45;
 const g_book_height = 75;
@@ -22,6 +48,7 @@ function draw_one_series_in_svg(p_series, p_position)
    let series_width = (g_book_width * books_count) + (g_series_padding * (books_count + 1));
 
    let series_title_x = 0;
+   let series_title_width = Math.max(400, series_width);
    let series_title_y = -(g_series_height/2) + g_series_title_height / 2;
 
    let book_start_x = -(series_width / 2);
@@ -30,8 +57,13 @@ function draw_one_series_in_svg(p_series, p_position)
    a.push(`<g transform="translate(${p_position.x},${p_position.y})">`);
    a.push(`   <g>`);
    a.push(`      <rect x="${-(series_width/2)}" y="${-(g_series_height/2)}" width="${(series_width)}" height="${(g_series_height)}" class="cssSeries" filter="url(#ID_BackgroundShadow)"/>`);
+/*
    a.push(`      <text x="${series_title_x}" y="${series_title_y}" dominant-baseline="middle" text-anchor="middle" class="cssSeriesShadow">${p_series.name}</text>`);
    a.push(`      <text x="${series_title_x}" y="${series_title_y}" dominant-baseline="middle" text-anchor="middle" class="cssSeries">${p_series.name}</text>`);
+*/
+
+
+   a.push(create_svg_foreign_object(series_title_x, series_title_y, series_title_width, g_series_title_height, `<b>${p_series.name}</b>`, "cssSeriesTitle"));
 
    for(let i = 0; i < p_series.books.length; ++i)
    {
